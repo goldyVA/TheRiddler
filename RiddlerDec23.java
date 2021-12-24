f8bc66270f178a365142e7c6d58f5293b7fa980d
Steven Goldburg <59116258+goldyvb@users.noreply.github.com>
2021-12-23 19:54:50 -0500

    Update RiddlerDec23.java

--- RiddlerDec23.java
+++ RiddlerDec23.java
@@ -1,3 +1,37 @@
+/* 
+Found all permutations of the letters of HAPPY HOLIDAYS (HAPYOLIDSX)
+and since there are only nine letters, I added an X for the 10th. 
+For each permutation, I assigned the position of each letter starting at 0
+to the letter, converted it to an integer, and did the computation.
+If the resulting HAPPY + HOLIDAYS = HOHOHOHO, print the result.
+
+Result 1:
+H -> 8
+A -> 4
+P -> 6
+Y -> 1
+O -> 0
+L -> 7
+I -> 2
+D -> 3
+S -> 9
+
+84661 + 80723419 = 80808080
+
+Result 2:
+H -> 6
+A -> 8
+P -> 3
+Y -> 2
+O -> 1
+L -> 5
+I -> 4
+D -> 7
+S -> 9
+
+68332 + 61547829 = 61616161
+
+*/
 
 public class RiddlerDec23 
 {
@@ -10,8 +44,8 @@ public class RiddlerDec23
 	}
 	
 
- private static void permute(String str, 
-                      int l, int r) 
+//permutation framework by Mihir Joshi at https://www.geeksforgeeks.org/java-program-to-print-all-permutations-of-a-given-string/
+ private static void permute(String str, int l, int r) 
  { 
      if (l == r) 
      {
